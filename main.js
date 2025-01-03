e4a283931ccd4c9fc489de13b766362d13b70b20
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-01-03 12:01:11 +0000

    Added the alert-user button

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ function print_to_console() {
     const norm_style = "color: white; backround-color: blue;";
     console.log("%cThis is the%c world we%c live in %cright now", norm_style, "color: red; background-color: green;", norm_style, "color: blue; background-color: pink;");
 }
+function alert_user() {
+    alert("Someone clicked on a button. Was that you?");
+}
